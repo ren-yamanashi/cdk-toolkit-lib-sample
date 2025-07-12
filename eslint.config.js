1c5a7dfeafb0985d049475d32b5c5997f45cd228
ren-yamanashi <pinokionigg@icloud.com>
2025-07-12 12:29:07 +0900

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,20 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
-import functional from 'eslint-plugin-functional';
 import cdkPlugin from 'eslint-cdk-plugin';
+import prettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   prettier,
   ...tseslint.configs.recommendedTypeChecked,
   {
+    files: ['**/*.ts'],
     languageOptions: {
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-    extends: [
-      functional.configs.recommended,
-      cdkPlugin.configs.recommended,
-    ]
-  },
-  {
-    files: ['**/*.test.ts', '**/*.spec.ts'],
-    rules: {
-      'functional/no-expression-statements': 'off',
-      'functional/no-return-void': 'off',
-    },
+    extends: [cdkPlugin.configs.recommended],
   }
 );
